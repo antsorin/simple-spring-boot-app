7eba3ee1b6f9c836a9dc7a9de2506c13241286ea
Sorin <antsorin@yahoo.com>
2022-04-21 16:48:23 +0300

    Using cookies for storing the theme preferences

--- src/main/java/com/example/simpleapp/portfolio/PortfolioController.java
+++ src/main/java/com/example/simpleapp/portfolio/PortfolioController.java
@@ -3,15 +3,12 @@ package com.example.simpleapp.portfolio;
 import java.sql.SQLException;
 import java.util.List;
 
-import javax.servlet.http.HttpSession;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class PortfolioController {
@@ -20,31 +17,23 @@ public class PortfolioController {
 	private CommentRepository commentsRepository;
 
 	@GetMapping("/portfolio")
-	public String getPortfolio(Model model, HttpSession session,
-			@RequestParam(required = false, name = "theme") String theme) throws SQLException {
+	public String getPortfolio(Model model) throws SQLException {
 
-		prepareSessionAndModelData(model, session, theme);
+		prepareModelData(model);
 
 		return "portfolio-template";
 	}
 
 	@PostMapping("/portfolio")
-	public String createPortfolio(@ModelAttribute Comment comment, Model model, HttpSession session,
-			@RequestParam(required = false, name = "theme") String theme) throws SQLException {
+	public String createPortfolio(@ModelAttribute Comment comment, Model model) throws SQLException {
 
 		commentsRepository.save(comment);
-
-		prepareSessionAndModelData(model, session, theme);
+		prepareModelData(model);
 
 		return "portfolio-template";
 	}
 
-	private void prepareSessionAndModelData(Model model, HttpSession session, String theme) throws SQLException {
-		if (theme != null) {
-			session.setAttribute("theme", theme);
-		}
-		model.addAttribute("theme", session.getAttribute("theme"));
-
+	private void prepareModelData(Model model) throws SQLException {
 		List<Comment> comments = commentsRepository.findAll();
 		model.addAttribute("comments", comments);
 	}
